4cca8d2e1e8519014abe4c7cbb0a28f3993927ee
Enis Amuk <enis.amuk@gmail.com>
2019-10-01 15:41:44 +0300

    Create Main.java

---
+++ naivebayes/Main.java
@@ -0,0 +1,11 @@
+package naivebayes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NaiveBayes naiveBayes = new NaiveBayes();
+		naiveBayes.calculateNaiveBayes();
+	}
+
+}
